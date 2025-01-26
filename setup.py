7f4d2c1df368ebdfdc5f36737a5127a8c3a42fb8
liquidcatmofu <liquidcatmofu@gmail.com>
2025-01-26 13:22:45 +0900

    setup.pyを更新、ver0.1.0に

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="discord_tts",
-    version="0.0.1",
+    version="0.1.0",
     description="Discord Text to Speech bot",
-    requires=["py-cord", "python-dotenv", "PyNaCl", "requests"],
+    requires=["py-cord", "python-dotenv", "PyNaCl", "requests", "google-re2"],
     packages=["discord_tts"],
     entry_points={
         "console_scripts": [
